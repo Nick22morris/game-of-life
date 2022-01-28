f9100487309afa162f11d2f481f4aacd58c19509
Nick <65043773+Nick22morris@users.noreply.github.com>
2022-01-28 09:04:15 -0600

    Made sexier (it was already pretty awesome)

--- runner.java
+++ runner.java
@@ -1,16 +1,10 @@
 import javax.swing.JOptionPane;
-
 public class runner {
 	public static void main(String[] args) {
-		Life kanye = new Life(50, 50, 0.50);
-		kanye.build();
-		int n = 0;
-		int count = 0;
-		while (n == 0) {
-			JOptionPane.showMessageDialog(null, count);
-			kanye.update();
-			kanye.print();
-			count++;
+		Life life = new Life(50, 50, 0.5);
+		//System.out.print(kanye.isAlive(0, 0));
+		while (true) {
+			life.nextGeneration();
 		}
 	}
 }
